3e63bef3327b009649d2b16055f2fb07dbdcd06d
Vladyslav Atanov <gibepromt@gmail.com>
2024-02-07 19:02:00 +0200

    Add user fix

--- src/lib/action.js
+++ src/lib/action.js
@@ -64,7 +64,7 @@ export const addUser = async (previousState, formData) => {
     const newUser = new User({
       username, 
       email,
-      hashedPassword, 
+      password: hashedPassword, 
       img,
       isAdmin,
     })
